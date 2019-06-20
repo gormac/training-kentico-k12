b656acde3f68e2399c993f793fa5ee93def8f57a
Jan Lenoch <jan.lenoch@hotmail.cz>
2019-06-20 14:38:19 +0200

    Drop an unused method

--- Business/Services/Errors/ErrorHelperService.cs
+++ Business/Services/Errors/ErrorHelperService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web.Mvc;
 
 using CMS.EventLog;
 using CMS.SiteProvider;
@@ -8,15 +7,6 @@ namespace Business.Services.Errors
 {
     public class ErrorHelperService : BaseService, IErrorHelperService
     {
-        public int UnprocessableStatusCode => 422;
-
-        public HttpStatusCodeResult HandleException(string source, string eventCode, Exception exception, int statusCode = 500)
-        {
-            LogException(source, eventCode, exception);
-
-            return new HttpStatusCodeResult(statusCode);
-        }
-
         public void LogException(string source, string eventCode, Exception exception) =>
             EventLogProvider.LogException(source, eventCode, exception, SiteContext.CurrentSiteID);
     }
--- Business/Services/Errors/IErrorHelperService.cs
+++ Business/Services/Errors/IErrorHelperService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web.Mvc;
 
 namespace Business.Services.Errors
 {
@@ -8,20 +7,6 @@ namespace Business.Services.Errors
     /// </summary>
     public interface IErrorHelperService : IService
     {
-        /// <summary>
-        /// HTTP status code of an unprocessable entity.
-        /// </summary>
-        int UnprocessableStatusCode { get; }
-
-        /// <summary>
-        /// Takes care of logging an exception as well as returning the required status code.
-        /// </summary>
-        /// <param name="source">The source of the exception to be logged in the Kentico event log.</param>
-        /// <param name="exception">The exception to log.</param>
-        /// <param name="statusCode">The HTTP status code to return.</param>
-        /// <returns>The HTTP status code</returns>
-        HttpStatusCodeResult HandleException(string source, string eventCode, Exception exception, int statusCode = 500);
-
         /// <summary>
         /// Logs an exception to the Kentico event log.
         /// </summary>
